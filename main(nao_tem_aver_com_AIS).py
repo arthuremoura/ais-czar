789c6640cadc440ce4e3f427ac1d550659ca2175
Arthur Moura <98397189+arthuremoura@users.noreply.github.com>
2025-11-23 23:45:43 -0300

    Update main(nao_tem_aver_com_AIS).py

--- main(nao_tem_aver_com_AIS).py
+++ main(nao_tem_aver_com_AIS).py
@@ -1,1 +1,42 @@
+import serial
+from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import FfmpegOutput
+from time import sleep
+import threading
+import json
+import queue
+import sys
 
+
+# Configuração da porta serial
+while True:
+    try:
+        ser = serial.Serial('/dev/ttyS0', 9600, timeout=0)
+        break
+    except:
+        pass
+
+  nome_id = 1
+# Variável para indicar quando o código de processamento está em execução
+processing_running = False
+
+# Fila para armazenar comandos desconhecidos
+data_receiver_queue= queue.Queue()
+
+
+# Função para ativar a câmera e gravar missão 1 
+def start1():
+    picam.start_recording(encoder, output)
+
+# Função para parar a gravação missão 1
+def stop1():
+    picam.stop_recording()
+  
+# Função para ativar a câmera e gravar missão 2 
+def start2():
+    picam.start_recording(encoder, output)
+
+# Função para parar a gravação missão 2
+def stop2():
+    picam.stop_recording()
